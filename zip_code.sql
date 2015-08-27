7adbab0b7708d53732173d054852702ec375124b
bo <keymork@163.com>
2015-08-27 23:18:19 +0800

    add create table sql
    add create table sql

---
+++ zip_code.sql
@@ -0,0 +1,7 @@
+CREATE TABLE `zip_code` (
+  `code` varchar(255) DEFAULT NULL,
+  `province` varchar(255) DEFAULT NULL,
+  `city` varchar(255) DEFAULT NULL,
+  `district` varchar(255) DEFAULT NULL,
+  `area` varchar(255) DEFAULT NULL
+) DEFAULT CHARSET=utf8;
